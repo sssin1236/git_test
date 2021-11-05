9589eeab6aa558c6c9e8fb2259cc3100150c1652
sssin1236 <suj0358@gmail.com>
2021-11-05 15:43:33 +0900

    on 스타일링

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ $(".prev").on("click", function(e){
     e.preventDefault();
     
     $(".panel li").last().prependTo(".panel");
+    $(".panel").removeClass("on");
+    $(".panel li").eq(2).addClass("on");
 });
 
 $(".next").on("click", function(e){
     e.preventDefault();
 
     $(".panel li").first().appendTo(".panel");
+    $(".panel").removeClass("on");
+    $(".panel li").eq(2).addClass("on");
 });
